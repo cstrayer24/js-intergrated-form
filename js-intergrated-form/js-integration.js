068f5a7d668b7ed3d6d9c7b8bab7cec1aab649c3
[Cohen <cstrayer4303@gmail.com>
2022-11-16 00:50:00 -0500

    reworked

--- js-intergrated-form/js-integration.js
+++ js-intergrated-form/js-integration.js
@@ -1,83 +1,53 @@
-//declarations
-    const calculator = document.querySelector('#calculator');
-    const num0 = document.getElementById('0');
-    const num1 = document.getElementById('1');
-    const num2 = document.getElementById('2');
-    const num3 = document.getElementById('3');
-    const num4 = document.getElementById('4');
-    const num5 = document.getElementById('5');
-    const num6 = document.getElementById('6');
-    const num7 = document.getElementById('7');
-    const num8 = document.getElementById('8');
-    const num9 = document.getElementById('9');
-    const plus = document.querySelector('#add');
-    const minus = document.querySelector('#subtract');
-    const multiply = document.querySelector('#multiply');
-    const divide = document.querySelector('#divide');
-    const CE = document.querySelector('#CE');
-    const buttonarr = [num0,num1,num2,num3,num4,num5,num6,num7,num8,num9];
-    const wrapper=document.querySelector('#wrapper');
 
-const p = document.getElementsByTagName('p');
+const valuebox= document.querySelector('#noOfclicks');
+const main = document.querySelector('#main');
 
-    //typing numbers
-    function typing(){
-    buttonarr.forEach(button=>button.addEventListener('click',function(){
+let clicks = 0;
 
-        console.log(wrapper.innerHTML);
-    }))
-    num0.addEventListener('click',function(){
+let value = valuebox.textContent;
 
-        wrapper.appendChild(document.createElement('p')).append(num0.innerText);
+
+
+class addclicks{
+
+
+
+    constructor(){
+
+        this.main = main;
+        this.valuebox = valuebox;
+        this.clicks = clicks;
+        this.value = valuebox.textContent;
+
+        main.addEventListener('click',function(){
+        
+        clicks++;
+      valuebox.textContent = clicks.toString();
+        console.log(clicks);
     });
-    num1.addEventListener('click',function(){
-
-        wrapper.appendChild(document.createElement('p')).append(num1.innerText);
-    });
-    num2.addEventListener('click',function(){
-
-        wrapper.appendChild(document.createElement('p')).append(num2.innerText);
-    });
-    num3.addEventListener('click',function(){
-
-        wrapper.appendChild(document.createElement('p')).append(num3.innerText);
-    });
-    };
-
-    typing();
-const children = wrapper.childNodes;
-
-    const clear = ()=>{
-
-        CE.addEventListener('click',function(){
-
-            children.forEach(p=>p.replaceWith(''));
-
-
-        })
-
-
-    }
-
-    clear();
+   
 
 
 
+}
+
+ }
+
+
+ 
+ 
+
+
+class double{}
 
 
 
+class auto{}
+
+class clicker{
 
 
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
+new addclicks;
