66d23eb5193e5737cf401183876d8df3639797c1
[Cohen <cstrayer4303@gmail.com>
2022-11-20 17:17:19 -0500

    simple form

--- js-intergrated-form/js-integration.js
+++ js-intergrated-form/js-integration.js
@@ -1,42 +1,11 @@
+const submit = document.querySelector('#submit');
+const textBox = document.querySelector('#textbox');
+const userOutput = document.querySelector('#output');
 
+submit.addEventListener('click',function(){
+    let userInput = textBox.value;
+    userOutput.innerHTML = `${userInput}`;
 
+    console.log(userInput);
 
-const clickDisplay = document.querySelector('#noOfclicks');
-let clicks  = 0;
-const main = document.querySelector('#main');
-const auto = document.querySelector('#auto');
-
-const settings = {
-
-    autoed:false
-}
-
-clickDisplay.innerHTML = clicks.toString();
-
-
-
-
-
-main.addEventListener('click',function(){
-    
-    clicks++;
-    
-    
-    clickDisplay.innerHTML = clicks.toString();
-    
-});
-window.setInterval(() => {
-    if(clicks>=15){
-
-       settings.autoed=true;
-    }
-}, 1);
-
-
-setInterval(()=>{
-
-    if(settings.autoed===true){
-
-       auto.style.backgroundColor = 'red';
-    };
 });
