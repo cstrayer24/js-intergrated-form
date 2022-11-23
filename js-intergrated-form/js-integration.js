8e30c9f93c5ac4a03a94ab621c3162522eaddf96
[Cohen <cstrayer4303@gmail.com>
2022-11-22 20:00:55 -0500

    made changes

--- js-intergrated-form/js-integration.js
+++ js-intergrated-form/js-integration.js
@@ -1,11 +1,16 @@
+
 const submit = document.querySelector('#submit');
 const textBox = document.querySelector('#textbox');
 const userOutput = document.querySelector('#output');
-
+const wrapper =document.querySelector('main');
+const color = document.querySelector('#color');
 submit.addEventListener('click',function(){
+
+
     let userInput = textBox.value;
     userOutput.innerHTML = `${userInput}`;
 
     console.log(userInput);
 
 });
+
